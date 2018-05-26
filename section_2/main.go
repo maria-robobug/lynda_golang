2b349a9669dc679fc0b382ff60babeccba8c0dae
maria-robobug <mariashow2001@gmail.com>
2018-05-26 19:16:09 +0100

    Use negroni and mux

--- section_2/main.go
+++ section_2/main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"encoding/json"
 	"encoding/xml"
-	"fmt"
 	"html/template"
 	"io/ioutil"
 	"net/http"
 	"net/url"
+
+	"github.com/codegangsta/negroni"
 
 	"database/sql"
 
@@ -41,12 +42,24 @@ type classifyBookResponse struct {
 	} `xml:"recommendations>ddc>mostPopular"`
 }
 
+var db *sql.DB
+
+func verifyDatabase(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	if err := db.Ping(); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	next(w, r)
+}
+
 func main() {
 	templates := template.Must(template.ParseFiles("templates/index.html"))
 
-	db, _ := sql.Open("sqlite3", "dev.db")
+	db, _ = sql.Open("sqlite3", "dev.db")
+	mux := http.NewServeMux()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		p := page{Name: "Gopher"}
 
 		if name := r.FormValue("name"); name != "" {
@@ -60,7 +73,7 @@ func main() {
 		}
 	})
 
-	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
 		results, err := search(r.FormValue("search"))
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -72,12 +85,9 @@ func main() {
 		}
 	})
 
-	http.HandleFunc("/books/add", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/books/add", func(w http.ResponseWriter, r *http.Request) {
 		book, err := find(r.FormValue("id"))
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-		if err = db.Ping(); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 
@@ -89,7 +99,10 @@ func main() {
 		}
 	})
 
-	fmt.Println(http.ListenAndServe(":8080", nil))
+	n := negroni.Classic()
+	n.Use(negroni.HandlerFunc(verifyDatabase))
+	n.UseHandler(mux)
+	n.Run(":8080")
 }
 
 func find(id string) (classifyBookResponse, error) {
